ec695feda8283aadee17d64a521dc9df70a8042f
RjayL <40307426+RjayL@users.noreply.github.com>
2018-06-15 16:08:14 -0700

    Add files via upload

---
+++ sencon.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+function test(id, val){
+  if(id == "processor"){
+    document.getElementById("gpu").style.visibility = "visible";
+  }
+  else {
+
+  }
+}
+</script>
